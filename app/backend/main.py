06123c25e54fd1e9649a4ca667774329b31efef5
JaredP45 <52184578+JaredP45@users.noreply.github.com>
2022-05-02 17:41:28 -0500

    Add new requirements; update frontend and backend main files to include asyncronous model serving.

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,6 +1,9 @@
 # backend/main.py
-
+import asyncio
+import time
 import uuid
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
 
 import cv2
 import uvicorn
@@ -15,18 +18,40 @@ import inference
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome from the API"}
 
+
 @app.post("/{style}")
-def get_image(style: str, file: UploadFile = File(...)):
+async def get_image(style: str, file: UploadFile = File(...)):
     image = np.array(Image.open(file.file))
     model = config.STYLES[style]
+    start = time.time()
     output, resized = inference.inference(model, image)
     name = f"/storage/{str(uuid.uuid4())}.jpg"
     cv2.imwrite(name, output)
-    return {"name": name}
+    models = config.STYLES.copy()
+    del models[style]
+    asyncio.create_task(generate_remaining_models(models, image, name))
+    return {"name": name, "time": time.time() - start}
+
+
+async def generate_remaining_models(models, image, name: str):
+    executor = ProcessPoolExecutor()
+    event_loop = asyncio.get_event_loop()
+    await event_loop.run_in_executor(
+        executor, partial(process_image, models, image, name)
+    )
+
+
+def process_image(models, image, name: str):
+    for model in models:
+        output, resized = inference.inference(models[model], image)
+        name = name.split(".")[0]
+        name = f"{name.split('_')[0]}_{models[model]}.jpg"
+        cv2.imwrite(name, output)
 
 
 if __name__ == "__main__":
